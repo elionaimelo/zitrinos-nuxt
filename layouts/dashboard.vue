c3b8c487a2ece1fcd9d835a96e774d493a3ffb6e
Elionai Melo <elionai.melo@lais.huol.ufrn.br>
2022-11-02 09:52:46 -0300

    style: add dashboard e sass loader

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -5,7 +5,7 @@ useHead({
   title: `${route.meta.title} - Zitrinos`,
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
   charset: "utf-8",
-  meta: [{ name: "description", content: "Zitrinos" }],
+  meta: [{ name: "description", content: `${route.meta.title} - Zitrinos` }],
 });
 </script>
 <template>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,3 +1,9 @@
+<script lang="ts" setup>
+definePageMeta({
+  title: "Dashboard",
+});
+</script>
+
 <template>
   <div>dashboard</div>
 </template>
